40c56f493b1ac63ff7ed083aee69ad7b98045a57
Ed-cred <edwardandrew63@gmail.com>
2023-09-28 15:14:51 +0300

    setup boltDB connection and filepath

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/Ed-cred/tasker/cmd"
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/Ed-cred/tasker/cmd"
+	"github.com/Ed-cred/tasker/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbPath := filepath.Join(home, "tasker.db")
+	err = db.Init(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 	cmd.RootCmd.Execute()
 }
